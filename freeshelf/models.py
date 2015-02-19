41f2c2a8d7f3c42ebfb78786e299da6db4deea97
Clinton Dreisbach <clinton@dreisbach.us>
2015-02-18 22:18:28 -0500

    Added User model

--- freeshelf/models.py
+++ freeshelf/models.py
@@ -9,3 +9,13 @@ class Book(db.Model):
 
     def __repr__(self):
         return "<Book {}>".format(self.title)
+
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    name = db.Column(db.String(255), nullable=False)
+    email = db.Column(db.String(255), unique=True, nullable=False)
+    encrypted_password = db.Column(db.String(60))
+
+    def __repr__(self):
+        return "<User {}>".format(self.email)

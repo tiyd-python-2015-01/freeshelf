7bf9f68ab5f531b7ec74ad581ec64fdda91f5ebe
Clinton Dreisbach <clinton@dreisbach.us>
2015-02-21 15:57:55 -0500

    Add logout functionality

--- freeshelf/views.py
+++ freeshelf/views.py
@@ -1,5 +1,5 @@
 from flask import render_template, flash, redirect, request, url_for
-from flask.ext.login import login_user, login_required, current_user
+from flask.ext.login import login_user, login_required, current_user, logout_user
 
 from . import app, db
 from .forms import LoginForm, RegistrationForm
@@ -31,8 +31,14 @@ def login():
         else:
             flash("That email or password is not correct.")
 
-    flash_errors(form)
     return render_template("login.html", form=form)
+
+
+@app.route("/logout", methods=["GET", "POST"])
+def logout():
+    logout_user()
+    flash("Logged out successfully.")
+    return redirect(url_for("index"))
 
 
 @app.route("/register", methods=["GET", "POST"])
@@ -55,6 +61,7 @@ def register():
         flash_errors(form)
 
     return render_template("register.html", form=form)
+
 
 @app.route("/favorite", methods=["POST"])
 @login_required
--- tests/test_user_management.py
+++ tests/test_user_management.py
@@ -1,4 +1,15 @@
+import pytest
+
 from freeshelf.models import User
+
+
+@pytest.fixture
+def user(session):
+    user = User(name="test", email="test@example.org", password="password")
+    session.add(user)
+    session.commit()
+    return user
+
 
 def test_register(client):
     response = client.post('/register', data=dict(
@@ -11,14 +22,23 @@ def test_register(client):
     assert "registered and logged in" in str(response.data)
 
 
-def test_login(client, session):
-    user = User(name="test", email="test@example.org", password="password")
-    session.add(user)
-    session.commit()
-
-    response = client.post('/login', data=dict(
-        email="test@example.org",
-        password="password"
+def login(user, client):
+    return client.post('/login', data=dict(
+        email=user.email,
+        password=user.password
     ), follow_redirects=True)
 
+
+def test_login(user, client):
+    response = login(user, client)
+
     assert "Logged in" in str(response.data)
+
+
+def test_logout(user, client):
+    login(user, client)
+
+    response = client.get('/logout',
+                          follow_redirects=True)
+
+    assert "Logged out" in str(response.data)

1480fce512b5eb575bdbc5bb7ce3e5a276e8b30a
Clinton Dreisbach <clinton@dreisbach.us>
2015-02-25 10:40:44 -0500

    Fix login view

--- freeshelf/__init__.py
+++ freeshelf/__init__.py
@@ -33,6 +33,6 @@ def create_app():
     migrate.init_app(app, db)
     bcrypt.init_app(app)
     login_manager.init_app(app)
-    login_manager.login_view = "login"
+    login_manager.login_view = "users.login"
 
     return app
